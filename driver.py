d70c2b4632fcc505cbfb04708fa57e191c680fa2
Hiroshi Furuya <furuyahir@gmail.com>
2015-09-12 16:17:44 -0400

    more updates to the .py

--- driver.py
+++ driver.py
@@ -4,38 +4,43 @@ import threading
 from math import *
 import datetime
 
-class TimerWorkerThread (threading.Thread):
-	def __init__(self, arg):
-		super(TimerWorkerThread, self).__init__()
-		self.event_loop = TimerEventLoop(arg)
-
-	def run(self):
-		self.event_loop.start()
-
-class TimerEventLoop():
-
-	def __init__(self, arg):
-		super(TimerEventLoop, self).__init__(arg)
-		self.deployed = False
-
-	def 
-
 def timerLoop(prevMinute):
 	curr_sched = f.get('curr_sched')
 	timers = f.get('/schedules', curr_sched)
+	currTime = datetime.datetime.now()
 	dOfWeek = date.today().weekday()
-	currHour = datetime.now().hour
-	currMinute = datetime.now().minute
+	currHour = datetime.datetime.now().hour
+	currMinute = datetime.datetime.now().minute
 	if currMinute != prevMinute:
 		prevMinute = currMinute
 		for block in timers:
-			if block["days"]==dOfWeek
+			if block["start_stop_posix"][1] < time.mktime(currTime.timetuple()):
+				f.delete('block')
+
+			elif block["start_stop_posix"][0] > time.mktime(currTime.timetuple()):
+			
+			elif block["days"]==dOfWeek
 				times = block["times"]
 				for eachTime in times:
 					hour = eachTime[0:1]
 					minute = eachTime[2:3]
 					if hour = currHour && minute = currMinute:
-						
+						curr_temp = f.get('curr_temp')
+						curr_humidity = f.get('curr_humidity')
+						duration_on = block["duration"]["time_sec"]
+						if curr_temp < block["duration"]["temp_range_F"][0]:
+							duration_on = duration_on * (1-block["duration"]["cold_percent"])
+						elif curr_temp > ["duration"]["temp_range_F"][1]:
+							duration_on = duration_on * (1+block["duration"]["hot_percent"])
+						if block["frequency"] is None:
+							t = Thread(target = oneTimeOn, args=(duration_on,))
+						else:
+							t = Thread(target = freqOn, args=(duration_on, block["frequency"]["every_min"], block["frequency"]["repeat_length_min"]))
+
+def oneTimeOn(duration):
+
+
+def freqOn (duration, min_repeat, repeatLength_min):
 
 
 
@@ -44,6 +49,4 @@ token = create_token("1yh4UVShYCta7gulWb7WZRQFgKldrPVrWny65nRk", auth_payload)
 
 
 f = Firebase('https://mhacks6.firebaseio.com')
-schedules = f.get('/schedules')
-for schedule in schedules:
 
